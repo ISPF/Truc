d46c2ec50f7510205593cbe3f208a67efe8df57d
Laurent PELLET <laurentp@ispf.pf>
2021-04-15 12:02:10 -1000

    Doc sur le vote

--- S01E02.R
+++ S01E02.R
@@ -1,13 +1,17 @@
+#https://en.wikipedia.org/wiki/Instant-runoff_voting
+#https://fr.wikipedia.org/wiki/Vote_%C3%A0_second_tour_instantan%C3%A9
+
 library(votesys)
 library(data.table)
 
 sujets <- fread("S01E02/sujets.csv")
 nbSujets <- nrow(sujets)
 
-#votes <- as.matrix(fread("S01E02/votes.csv"))
-votes <- matrix(nrow = 100,ncol=nbSujets)
-votes <- t(mapply(1:nrow(votes), FUN=function(x)
-  sample(1:nbSujets,nbSujets,replace=FALSE)))
+#votes <- matrix(nrow = 100,ncol=nbSujets)
+#votes <- t(mapply(1:nrow(votes), FUN=function(x)
+#  sample(1:nbSujets,nbSujets,replace=FALSE)))
+
+votes <- as.matrix(fread("S01E02/votes.csv"))
 colnames(votes) <- sujets$Sujet
 votes
 
@@ -17,8 +21,7 @@ resultatVote$winner
 
 #check_dup_wrong()
 
-#https://en.wikipedia.org/wiki/Instant-runoff_voting
-#https://fr.wikipedia.org/wiki/Vote_%C3%A0_second_tour_instantan%C3%A9
+
 
 #y <- cdc_simple(vote)
 #y <- cdc_rankedpairs(vote)
