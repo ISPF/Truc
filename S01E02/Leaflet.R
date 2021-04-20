688901c33673fe9fc27bd17a23e565acb16d0971
Laurent PELLET <laurentp@ispf.pf>
2021-04-20 08:18:58 -1000

    S01E02 et Leaflet

---
+++ S01E02/Leaflet.R
@@ -0,0 +1,30 @@
+library(leaflet)
+library(rgdal)
+
+#http://rstudio.github.io/leaflet/
+
+Comas <-readOGR("S01E02/shp", "Comas")
+#Comas <- readOGR("S01E02/geojson/comas.GeoJSON") # tres tres long
+
+pal <- colorNumeric(palette = "Reds",domain = Comas$Rsdncpr)
+
+leaflet(Comas) %>%
+  addTiles(group = "OSM (default)") %>%
+  addProviderTiles(providers$Stamen.Toner, group = "Toner") %>%
+  addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite") %>%
+  addProviderTiles("Esri.WorldImagery") %>%
+  addLayersControl(baseGroups = c("OSM (default)", "Toner", "Toner Lite", "Esri")) %>%
+  addPolygons(fillOpacity = 0.8,color = ~pal(Rsdncpr), weight = 2, 
+              popup = ~sprintf("<b>%s</b><br/>
+              Individus : %s<br/>
+              Logements : %s<br/>
+              Individus des résidences principales : %s<br/>
+              Résidences principales : %s",Comas,Indivds,Logmnts, Indvdrp, Rsdncpr)) %>%
+   addLegend("bottomright", pal = pal, values = ~Rsdncpr,title = "Res. Pr",opacity = 1)
+
+
+
+X <- Comas@data
+X$indice <- as.numeric(row.names(X))
+X$test <- X$indice/X$IDComas
+Comas@data <- X
